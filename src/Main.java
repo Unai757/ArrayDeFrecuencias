557bd682c0fe9bb27746e6462968e1f226309773
Unai75 <unai.crupas@educa.jcyl.es>
2026-03-04 13:05:36 +0100

    Mejoro codigo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int numeroMaximo=0;
-        int numeroConFrecuencia=0;
         Random rand = new Random();
         //Creamos los dos arrays
         int[] numeros = new int[10];
@@ -24,17 +23,33 @@ public class Main {
 
         System.out.println();
         System.out.println("Array con las frecuencias");
-        //Y mostramos la informacion de los dos array
+        //Hacemos este bucle para buscar cuál es el maximo
         for (int i = 0; i < numeros.length; i++) {
             System.out.print(i + ": "+frecuencia[i]+" ");
             if (numeroMaximo<frecuencia[i]) {
                 numeroMaximo=frecuencia[i];
-                numeroConFrecuencia=i;
+
+                }
+
             }
 
-        }
         System.out.println();
+        //Hacemos este bucle para que nos muestre todos los numeros que tengan la frecuencia maxima
+        for (int i = 0; i <frecuencia.length ; i++) {
+            if (frecuencia[i] == numeroMaximo) {
+                System.out.println("El numero que mas aparece es: " + i + " apareciendo estas veces: " + numeroMaximo);
+
+            }
+        }
+
+
+
+        }
+        /*System.out.println();
         System.out.println("El numero que mas aparece es: " + numeroConFrecuencia +" apareciendo estas veces: "+numeroMaximo);
+*/
+
+
     }
-}
+
 
